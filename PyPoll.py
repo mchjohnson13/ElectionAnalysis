aa6edb87eb903feb36a3e45d60d95a54ab9eb058
Michelle Johnson <Michelle.Johnson@domain.local>
2020-08-02 15:39:56 -0700

    3.5.5 updated complete

--- PyPoll.py
+++ PyPoll.py
@@ -73,6 +73,7 @@ winning_candidate_summary = (
     f"--------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n")
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"--------------------\n")
 
 print(winning_candidate_summary)
